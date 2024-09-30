4f319ea6884cf05717246dab79be23b4b933ab40
NomanNayeem5516 <nomannayeem5516@gmail.com>
2024-09-30 12:21:58 +0600

    add todays deal

--- lib/screens/bottom/home.dart
+++ lib/screens/bottom/home.dart
@@ -1,9 +1,10 @@
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
-
+import '../../models/featured.dart';
 import '../../models/slider.dart';
-import '../../networkManager/repository.dart'; // Adjust this import path accordingly
+import '../../models/todaysdeal.dart';
+import '../../networkManager/repository.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -14,25 +15,61 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List<String> sliderImages = [];
-  bool isLoading = true;
+  List<CategoryData> categories = [];
+  List<ProductData> todaysDealProducts = [];
+  bool isLoadingSliders = true;
+  bool isLoadingCategories = true;
+  bool isLoadingProducts = true;
 
   @override
   void initState() {
     super.initState();
     fetchSliders();
+    fetchFeaturedCategories();
+    fetchTodaysDealProducts();
   }
 
   Future<void> fetchSliders() async {
     try {
-      Sliders sliders = await Repository().getSliders(); // Fetching sliders from the repository
+      Sliders sliders = await Repository().getSliders(); // Fetching sliders
       setState(() {
         sliderImages = sliders.data!.map((slider) => slider.photo!).toList();
-        isLoading = false;
+        isLoadingSliders = false;
       });
     } catch (e) {
       // Handle error
       setState(() {
-        isLoading = false;
+        isLoadingSliders = false;
+      });
+    }
+  }
+
+  Future<void> fetchFeaturedCategories() async {
+    try {
+      Categories categoryResponse = await Repository().getFeaturedCategories(); // Fetching categories
+      setState(() {
+        categories = categoryResponse.data!;
+        isLoadingCategories = false;
+      });
+    } catch (e) {
+      // Handle error
+      setState(() {
+        isLoadingCategories = false;
+      });
+    }
+  }
+
+  Future<void> fetchTodaysDealProducts() async {
+    try {
+      Products products = await Repository().getTodaysDealProducts(); // Fetching today's deal products
+      setState(() {
+        todaysDealProducts = products.data!;
+        isLoadingProducts = false;
+      });
+    } catch (e) {
+      // Handle error
+      setState(() {
+        isLoadingProducts = false;
       });
     }
   }
@@ -41,70 +78,186 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[100],
-      body: Column(
-        children: [
-          SizedBox(
-            height: 120,
-            child: Stack(
-              children: [
-                Container(
-                  height: 100,
-                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-                  decoration: BoxDecoration(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Sliders Section
+            SizedBox(
+              height: 120,
+              child: Stack(
+                children: [
+                  Container(
+                    height: 100,
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                    decoration: BoxDecoration(
                       color: Colors.amberAccent,
                       borderRadius: BorderRadius.only(
                         bottomLeft: Radius.circular(30),
                         bottomRight: Radius.circular(30),
-                      )),
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Icon(Icons.arrow_back_ios_rounded,
-                              color: Colors.white, size: 30),
-                          Icon(Icons.search_outlined,
-                              color: Colors.white, size: 30)
-                        ],
                       ),
-                    ],
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Icon(Icons.arrow_back_ios_rounded, color: Colors.white, size: 30),
+                        Icon(Icons.search_outlined, color: Colors.white, size: 30),
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 100,
-            width: double.infinity,
-            child: isLoading
-                ? Center(child: CircularProgressIndicator()) // Loading state
-                : sliderImages.isEmpty
-                ? Center(child: Text("No sliders available")) // Empty state
-                : CarouselSlider(
-              options: CarouselOptions(
-                autoPlay: true,
-                enlargeCenterPage: true,
+                ],
               ),
-              items: sliderImages.map((imageUrl) {
-                return Builder(
-                  builder: (BuildContext context) {
-                    return Container(
-                      width: MediaQuery.of(context).size.width,
-                      margin: EdgeInsets.symmetric(horizontal: 5.0),
-                      decoration: BoxDecoration(
-                        color: Colors.amberAccent,
-                      ),
-                      child: Image.network(
-                        imageUrl,
-                        fit: BoxFit.cover,
-                      ),
-                    );
-                  },
-                );
-              }).toList(),
             ),
-          ),
-        ],
+            // Carousel Slider
+            SizedBox(
+              height: 100,
+              width: double.infinity,
+              child: isLoadingSliders
+                  ? Center(child: CircularProgressIndicator())
+                  : sliderImages.isEmpty
+                  ? Center(child: Text("No sliders available"))
+                  : CarouselSlider(
+                options: CarouselOptions(
+                  autoPlay: true,
+                  enlargeCenterPage: true,
+                ),
+                items: sliderImages.map((imageUrl) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.symmetric(horizontal: 5.0),
+                        decoration: BoxDecoration(color: Colors.amberAccent),
+                        child: Image.network(
+                          imageUrl,
+                          fit: BoxFit.cover,
+                        ),
+                      );
+                    },
+                  );
+                }).toList(),
+              ),
+            ),
+            SizedBox(height: 20),
+            // Featured Categories Section
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Text(
+                'Featured Categories',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: 60,
+              child: isLoadingCategories
+                  ? Center(child: CircularProgressIndicator())
+                  : categories.isEmpty
+                  ? Center(child: Text("No categories available"))
+                  : ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: categories.length,
+                itemBuilder: (context, index) {
+                  final category = categories[index];
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 40,
+                          width: 125,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.3),
+                                spreadRadius: 2,
+                                blurRadius: 5,
+                              ),
+                            ],
+                          ),
+                          child: Center(child: Text(category.name ?? "Unknown")),
+                        )
+
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 20),
+            // Today's Deal Products Section
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Text(
+                'Today\'s Deals',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: 250,
+              child: isLoadingProducts
+                  ? Center(child: CircularProgressIndicator())
+                  : todaysDealProducts.isEmpty
+                  ? Center(child: Text("No today's deal products available"))
+                  : ListView.builder(
+                //scrollDirection: Axis.horizontal,
+                itemCount: todaysDealProducts.length,
+                itemBuilder: (context, index) {
+                  final product = todaysDealProducts[index];
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          height: 150,
+                          width: 150,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.3),
+                                spreadRadius: 2,
+                                blurRadius: 5,
+                              ),
+                            ],
+                          ),
+                          child: product.thumbnailImage != null
+                              ? Image.network(
+                            product.thumbnailImage!,
+                            fit: BoxFit.cover,
+                          )
+                              : Icon(Icons.image),
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          product.name ?? "Product Name",
+                          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 4),
+                        Text(
+                          product.mainPrice ?? "\$0.00",
+                          style: TextStyle(color: Colors.green),
+                        ),
+                        product.hasDiscount ?? false
+                            ? Text(
+                          product.strokedPrice ?? "",
+                          style: TextStyle(
+                            color: Colors.red,
+                            decoration: TextDecoration.lineThrough,
+                          ),
+                        )
+                            : Container(),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
